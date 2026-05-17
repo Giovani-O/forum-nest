e602843d29d38115fb04b6a1a8536d399264a19c
Giovani <giovani98@proton.me>
2026-05-17 14:28:41 -0300

    chore: setup env and nest config module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import 'dotenv/config'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import 'dotenv/config'
 import { AppModule } from './app.module.js'
+import { Env } from './env.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false
   })
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
   app.getHttpAdapter().getInstance().set('json spaces', 2)
   app.enableShutdownHooks()
-  await app.listen(process.env.PORT ?? 3333)
+  await app.listen(port)
 }
 bootstrap()
