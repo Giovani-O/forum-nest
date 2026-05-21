6a464bee71524b4bb77ddf6529fcfac5be14760d
Giovani <giovani98@proton.me>
2026-05-21 13:50:55 -0300

    feat: Create question mapper

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -19,7 +19,7 @@ export interface QuestionProps {
   slug: Slug
   attachments: QuestionAttachmentList
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
 export class Question extends AggregateRoot<QuestionProps> {
