a6564e859514be0c6c4f56a11ef48507d87ce948
Giovani <giovani98@proton.me>
2026-05-21 17:28:07 -0300

    feat: WiP - Student entity + cryptography utils

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,23 @@ import type { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this._props.name
+  }
+
+  get email() {
+    return this._props.email
+  }
+
+  get password() {
+    return this._props.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
 
