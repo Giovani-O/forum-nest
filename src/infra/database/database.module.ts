123d3ef331bd3f3eb8ff44c4fac5d5f2a7a41bd3
Giovani <giovani98@proton.me>
2026-05-22 18:51:15 -0300

    refact: authenticate controller

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question.repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-coments-repository'
@@ -7,14 +8,16 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionRepository } from './prisma/repositories/prisma-question-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
+    { provide: QuestionsRepository, useClass: PrismaQuestionRepository },
+    { provide: StudentsRepository, useClass: PrismaStudentsRepository },
     PrismaService,
     PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentsRepository,
-    { provide: QuestionsRepository, useClass: PrismaQuestionRepository },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
   ],
@@ -23,9 +26,10 @@ import { PrismaQuestionRepository } from './prisma/repositories/prisma-question-
     PrismaAnswersRepository,
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentsRepository,
-    QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
+    QuestionsRepository,
+    StudentsRepository,
   ],
 })
 export class DatabaseModule {}
