2963339e73f6a0cb313a9bd5faa60888c8209cf4
Giovani <giovani98@proton.me>
2026-05-22 16:39:32 -0300

    feat: create cryptography module

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,13 @@
+import { compare, hash } from 'bcryptjs'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+export class BcryptHasher implements HashGenerator, HashComparer {
+  hash(plain: string): Promise<string> {
+    return hash(plain, 8)
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash)
+  }
+}
---
+++ src/infra/cryptography/jwt-encrypter.ts
@@ -0,0 +1,10 @@
+import { JwtService } from '@nestjs/jwt'
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+
+export class JwtEncrypter implements Encrypter {
+  constructor(private jwtService: JwtService) {}
+
+  encrypt(payload: Record<string, unknown>): Promise<string> {
+    return this.jwtService.signAsync(payload)
+  }
+}
