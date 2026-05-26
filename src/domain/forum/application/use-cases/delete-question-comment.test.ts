2dc22c0972f361ed614bd752f01fc228fe31af0a
Giovani <giovani98@proton.me>
2026-05-26 14:24:17 -0300

    refact: improve e2e tests

--- src/domain/forum/application/use-cases/delete-question-comment.test.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.test.ts
@@ -2,8 +2,8 @@ import { makeQuestionComment } from '@test/factories/make-question-comment.js'
 import { InMemoryQuestionCommentsRepository } from '@test/repositories/in-memory-question-comments-repository.js'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
-import { DeleteQuestionCommentUseCase } from './delete-question-comment.js'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed.error.js'
+import { DeleteQuestionCommentUseCase } from './delete-question-comment.js'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
 let sut: DeleteQuestionCommentUseCase
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,9 +1,12 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id.js'
 import {
   Question,
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question.js'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
@@ -13,11 +16,28 @@ export function makeQuestion(
     {
       authorId: new UniqueEntityID(),
       title: faker.lorem.sentence(),
-      content: faker.lorem.paragraph(),
+      content: faker.lorem.text(),
       ...override,
     },
     id,
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
